754f7aed01b343e7f5aab81ae74040fb545914a1
Sids0108 <siddharths5057@yahoo.com>
2026-02-21 19:45:17 +0530

    first commit

---
+++ src/main/java/com/jaga_kidney_backend/JagaKidneyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jaga_kidney_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagaKidneyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JagaKidneyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaga_kidney_backend/JagaKidneyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaga_kidney_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JagaKidneyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
